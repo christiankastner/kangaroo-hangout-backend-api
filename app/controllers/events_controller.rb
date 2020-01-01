013227080eda5b8a9d37079e743d1046d3f1e3f8
ckastner12 <github email address>
2020-01-01 12:04:19 -0800

    accepting nested params for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,12 @@ class EventsController < ApplicationController
 
     def create
         byebug()
-
+        
     end
 
     private
 
     def event_params
-        params.require(:event).permit(:date, :user_id, :activities)
+        params.require(:event).permit(:date, :user_id, activities: [:name, :formatted_address, :icon])
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
     has_many :activities
     has_many :attendees
     belongs_to :user
+    accepts_nested_attributes_for :activities
 end
