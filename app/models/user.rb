4351e86e35ee7ca89f9b71035c4c5be48871c94c
Christian Kastner <christianmkastner@gmail.com>
2020-02-11 13:41:46 -0800

    Added password digest to users model, reconfiguring users controller now

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :events, dependent: :destroy
     has_many :activities, through: :events
     has_many :attendees
+    validates :email, uniqueness: { case_sensitive: false }
     has_secure_password
 end
